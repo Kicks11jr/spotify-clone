28edeb03154144975add4be5a9d5d508af081ff1
Kicks11jr <pellet.kn11@gmail.com>
2023-11-14 03:42:30 +0100

    Update Providers for auth and supabase

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -3,7 +3,7 @@ import {
     useSessionContext, 
     useUser as useSupaUser 
 } from "@supabase/auth-helpers-react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState, useContext } from "react";
 
 import { Subscription, UserDetails } from "@/types";
 
@@ -63,7 +63,28 @@ export const MyUserContextProvider = (props: Props) => {
 
                     setIsLoadingData(false);
                 }
-            )
+            );
+        } else if (!user && !isLoadingUser && !isLoadingData) {
+            setUserDetails(null);
+            setSubscription(null);
         }
-    }, []);
-}
+    }, [user, isLoadingUser]);
+
+    const value = {
+        accessToken,
+        user,
+        userDetails,
+        isLoading: isLoadingUser || isLoadingData,
+        subscription
+    }
+
+    return <UserContext.Provider value={value} {...props}/>
+};
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+        throw new Error(`useUser must be used within a MyUserContextProvider.`);
+    }
+    return context;
+};

b8337ee2623a969b55a38bcfb4ef2e85a5f7c5b1
Kicks11jr <pellet.kn11@gmail.com>
2023-11-15 05:08:52 +0100

    adding the music list in the sidebar while being linked to supabase + card design

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,16 +4,21 @@ import { usePathname } from "next/navigation";
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+
+import { Song } from "@/types";
+
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
 import Library from "./Library";
 
 interface SidebarProps {
     children: React.ReactNode;
+    songs: Song[]
 }
 
 const Sidebar: React.FC<SidebarProps> = ({
-    children
+    children,
+    songs
 }) => {
     const pathname = usePathname();
 
@@ -44,26 +49,26 @@ const Sidebar: React.FC<SidebarProps> = ({
             w-[300px]
             p-2
             ">
-            <Box>
-                <div className="
-                flex
-                flex-col
-                gap-y-4
-                px-5
-                py-4
-                ">
-                    {routes.map((item) => (
-                    <SidebarItem
-                    key={item.label}
-                    {...item}
-                    />
-                    ))}
+                <Box>
+                    <div className="
+                    flex
+                    flex-col
+                    gap-y-4
+                    px-5
+                    py-4
+                    ">
+                        {routes.map((item) => (
+                        <SidebarItem
+                        key={item.label}
+                        {...item}
+                        />
+                        ))}
 
-                </div>
-            </Box>
-            <Box className="overflow-y-auto h-full">
-                <Library />
-            </Box>
+                    </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    <Library songs={songs}/>
+                </Box>
             </div>
             <main className="h-full flex-1 overflow-y-auto py-2">
                 {children}
